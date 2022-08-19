abc2687b654015ecf34f20db65838933c4003b39
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-08-19 22:05:26 +0300

    created a tridimentional  loop to automate pieces

--- src/components/pieces.js
+++ src/components/pieces.js
@@ -1,147 +1,67 @@
-const piece = {
-    pieceName:"",
-    image:undefined,
-    x:null,
-    y:null,
+const allPieces = [];
+
+class Piece{
+    constructor(id = "", image = undefined, x = null, y = null){
+        this.id = id;
+        this.image = image;
+        this.x = x;
+        this.y = y;
+    }
 };
 
-const pawn1_w = piece;
-pawn1_w.id = "pawn1_w";
-pawn1_w.image = "assets/pawn_w.png";
-const pawn2_w = piece;
-pawn2_w.id = "pawn2_w";
-pawn2_w.image = "assets/pawn_w.png";
-const pawn3_w = piece;
-pawn3_w.id = "pawn3_w";
-pawn3_w.image = "assets/pawn_w.png";
-const pawn4_w = piece;
-pawn4_w.id = "pawn4_w";
-pawn4_w.image = "assets/pawn_w.png";
-const pawn5_w = piece;
-pawn5_w.id = "pawn5_w";
-pawn5_w.image = "assets/pawn_w.png";
-const pawn6_w = piece;
-pawn6_w.id = "pawn6_w";
-pawn6_w.image = "assets/pawn_w.png";
-const pawn7_w = piece;
-pawn7_w.id = "pawn7_w";
-pawn7_w.image = "assets/pawn_w.png";
-const pawn8_w = piece;
-pawn8_w.id = "pawn8_w";
-pawn8_w.image = "assets/pawn_w.png";
-
-
-const pawn1_b = piece;
-pawn1_b.id = "pawn1_b";
-pawn1_b.image = "assets/pawn_b.png";
-const pawn2_b = piece;
-pawn2_b.id = "pawn2_b";
-pawn2_b.image = "assets/pawn_b.png";
-const pawn3_b = piece;
-pawn3_b.id = "pawn3_b";
-pawn3_b.image = "assets/pawn_b.png";
-const pawn4_b = piece;
-pawn4_b.id = "pawn4_b";
-pawn4_b.image = "assets/pawn_b.png";
-const pawn5_b = piece;
-pawn5_b.id = "pawn5_b";
-pawn5_b.image = "assets/pawn_b.png";
-const pawn6_b = piece;
-pawn6_b.id = "pawn6_b";
-pawn6_b.image = "assets/pawn_b.png";
-const pawn7_b = piece;
-pawn7_b.id = "pawn7_b";
-pawn7_b.image = "assets/pawn_b.png";
-const pawn8_b = piece;
-pawn8_b.id = "pawn8_b";
-pawn8_b.image = "assets/pawn_b.png";
-
-const rook1_w = piece;
-rook1_w.image = "assets/rook_w.png";
-rook1_w.id = "rook1_w";
-const rook2_w = piece;
-rook2_w.image = "assets/rook_w.png";
-rook2_w.id = "rook2_w";
-const rook1_b = piece;
-rook2_b.image = "assets/rook_b.png";
-rook1_b.id = "rook1_b";
-const rook2_b = piece;
-rook2_b.image = "assets/rook_b.png";
-rook2_b.id = "rook2_b";
-
-const knigt1_w = piece;
-knigt1_w.id = "knigt1_w";
-knigt1_w.image = "assets/knigt_w.png";
-const knigt2_w = piece;
-knigt2_w.id = "knigt2_w";
-knigt2_w.image = "assets/knigt_w.png";
-const knigt1_b = piece;
-knigt1_b.id = "knigt1_b";
-knigt2_b.image = "assets/knigt_b.png";
-const knigt2_b = piece;
-knigt2_b.id = "knigt2_b";
-knigt2_b.image = "assets/knigt_b.png";
-
-const bishop1_w = piece;
-bishop1_w.id = "bishop1_w";
-bishop1_w.image = "assets/bishop_w.png";
-const bishop2_w = piece;
-bishop2_w.id = "bishop2_w";
-bishop2_w.image = "assets/bishop_w.png";
-const bishop1_b = piece;
-bishop1_b.id = "bishop1_b";
-bishop1_b.image = "assets/bishop_b.png";
-const bishop2_b = piece;
-bishop2_b.id = "bishop2_b";
-bishop2_b.image = "assets/bishop_b.png";
-
-const queen_w = piece;
-queen_w.id = "queen_w";
-queen_w.image = "assets/queen_w.png";
-const queen_b = piece;
-queen_b.id = "queen_b";
-queen_b.image = "assets/queen_b.png";
-
-const king_w = piece;
-king_w.id = "king_w";
-king_w.image = "assets/king_w.png";
-const king_b = piece;
-king_b.id = "king_b";
-king_b.image = "assets/king_b.png";
-
-const allPieces = [
-    pawn1_w,
-    pawn2_w,
-    pawn3_w,
-    pawn4_w,
-    pawn5_w,
-    pawn6_w,
-    pawn7_w,
-    pawn8_w,
-    pawn1_b,
-    pawn2_b,
-    pawn3_b,
-    pawn4_b,
-    pawn5_b,
-    pawn6_b,
-    pawn7_b,
-    pawn8_b,
-    rook1_w,
-    rook2_w,
-    rook1_b,
-    rook2_b,
-    knigt1_w,
-    knigt2_w,
-    knigt1_b,
-    knigt2_b,
-    bishop1_w,
-    bishop2_w,
-    bishop1_b,
-    bishop2_b,
-    queen_w,
-    queen_b,
-    king_w,
-    king_b
+//we have 2 colors of pieces
+const piecesColor = [
+    "_w",
+    "_b"
 ];
+
+//each color contains this pieces, this many times.
+const piecesNames = [
+    ["pawn", 8],
+    ["rook", 2],
+    ["knigth", 2],
+    ["bishop", 2],
+    ["queen", 1],
+    ["king", 1],
+];
+
+for(let color = 0; color < piecesColor.length; color++){
+    for(let piecesType  = 0; piecesType < piecesNames.length; piecesType++ ){
+        for(let amount = piecesNames[piecesType][1]; amount > 0; amount--){
+
+            let id;
+
+            if(piecesNames[piecesType][0] === "queen" || piecesNames[piecesType][0] === "king"){
+                id = piecesNames[piecesType][0] + piecesColor[color];
+            }else{
+                id = piecesNames[piecesType][0] + amount + piecesColor[color];
+            }
+            
+            const image = `assets/${piecesNames[piecesType][0] + piecesColor[color]}.png`;
+            const currentPiece = new Piece(id, image);
+
+            allPieces.push(currentPiece);
+
+        }
+    }
+}
+allPieces.forEach(p => console.log(p.id, p.image))
+
+
+/*
+const pawns_w = Array(8).fill(piece).map((p, i) => {
+    p.id = `pawn${i+1}_w`;
+    p.image = "assets/pawn_w.png";
+    p.x = i;
+    p.y = 1;
+});
+
+const pawns_b = Array(8).fill(piece).map((p, i) => {
+    p.id = `pawn${i+1}_w`;
+    p.image = "assets/pawn_w.png";
+    p.x = i;
+    p.y = 1;
+});
+*/
 
 export default allPieces;
