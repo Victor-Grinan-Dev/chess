dd93110c2d8eccba1545f5b37af682d02daa2a30
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-01-02 11:49:48 +0200

    renamed app to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Chessboard from './components/Chessboard';
 function App() {
   return (
     <div className='app'>
+      <h1>Chess!</h1>
+      <p>points</p>
       <Chessboard/>
+      <p>points</p>
     </div>
   );
 }
